b232eca5928a562cbd21ff070e50c958a6f34563
Bretl7 <bret.landen97@gmail.com>
2022-07-18 15:21:31 -0700

    Added Birdie sprite

--- main.cs
+++ main.cs
@@ -3,11 +3,11 @@ using System;
 
 public class main : Node2D
 {
-    public override void _Ready()
-    {
-        if (OS.GetName() == "HTML5")
-            GetTree().ChangeScene("res://client/scenes/MainLogin/MainLogin.tscn");
-        else
-            GetTree().ChangeScene("res://server/scenes/ServerStaging.tscn");
-    }
+	public override void _Ready()
+	{
+		if (OS.GetName() == "HTML5")
+			GetTree().ChangeScene("res://client/scenes/MainLogin/MainLogin.tscn");
+		else
+			GetTree().ChangeScene("res://server/scenes/ServerStaging.tscn");
+	}
 }
